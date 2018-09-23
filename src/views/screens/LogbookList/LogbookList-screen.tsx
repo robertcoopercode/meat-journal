2b6d013663b5b9e73178d8bf5a25a9c88e9c7262
Robert Cooper <robert.cooper.qu@gmail.com>
2018-09-22 23:02:47 -0400

    Logbook list page styling

--- src/views/screens/LogbookList/LogbookList-screen.tsx
+++ src/views/screens/LogbookList/LogbookList-screen.tsx
@@ -12,7 +12,7 @@ export interface LogbookListScreenProps extends NavigationScreenProps<{}> {}
 
 const ROOT: ViewStyle = {
   backgroundColor: color.background,
-  flex: 1,
+  // flex: 1,
 }
 
 const LOG_ENTRY_GROUP = {
@@ -61,19 +61,121 @@ export class LogbookList extends React.Component<LogbookListScreenProps, {}> {
     return (
       <Screen style={ROOT} preset="scrollStack">
         <EntryGroup
-          date={`September 15`.toUpperCase()}
+          date={`Today`.toUpperCase()}
           entries={[
             {
-              type="cow"
+              type: "cow",
               name: "Ground Beef",
               weight: "1.2 lbs",
               time: "10:00 PM",
             },
             {
-              type="porc"
+              type: "porc",
               name: "Bacon",
               weight: "0.5 lbs",
               time: "10:30 PM",
+            },
+          ]}
+        />
+        <EntryGroup
+          date={`September 15`.toUpperCase()}
+          entries={[
+            {
+              type: "cow",
+              name: "Ground Beef",
+              weight: "1.0 lbs",
+              time: "9:00 PM",
+            },
+            {
+              type: "lamb",
+              name: "Lamb",
+              weight: "1.5 lbs",
+              time: "9:30 PM",
+            },
+          ]}
+        />
+        <EntryGroup
+          date={`September 14`.toUpperCase()}
+          entries={[
+            {
+              type: "cow",
+              name: "Beef Cubes",
+              weight: "2.0 lbs",
+              time: "9:00 PM",
+            },
+            {
+              type: "porc",
+              name: "Porc Chops",
+              weight: "0.5 lbs",
+              time: "9:15 PM",
+            },
+          ]}
+        />
+        <EntryGroup
+          date={`September 13`.toUpperCase()}
+          entries={[
+            {
+              type: "lamb",
+              name: "Lamb",
+              weight: "1.2 lbs",
+              time: "9:00 PM",
+            },
+            {
+              type: "porc",
+              name: "Ground Porc",
+              weight: "0.8 lbs",
+              time: "9:30 PM",
+            },
+          ]}
+        />
+        <EntryGroup
+          date={`September 12`.toUpperCase()}
+          entries={[
+            {
+              type: "horse",
+              name: "Horse Flank",
+              weight: "1.2 lbs",
+              time: "9:00 PM",
+            },
+            {
+              type: "cow",
+              name: "Steak",
+              weight: "1.2 lbs",
+              time: "9:30 PM",
+            },
+          ]}
+        />
+        <EntryGroup
+          date={`September 11`.toUpperCase()}
+          entries={[
+            {
+              type: "duck",
+              name: "Duck Breast",
+              weight: "0.8 lbs",
+              time: "9:00 PM",
+            },
+            {
+              type: "cow",
+              name: "Beef Flank",
+              weight: "1.4 lbs",
+              time: "9:30 PM",
+            },
+          ]}
+        />
+        <EntryGroup
+          date={`September 10`.toUpperCase()}
+          entries={[
+            {
+              type: "lamb",
+              name: "Lamb Chops",
+              weight: "0.4 lbs",
+              time: "9:00 PM",
+            },
+            {
+              type: "porc",
+              name: "Ground Porc",
+              weight: "2.2 lbs",
+              time: "9:30 PM",
             },
           ]}
         />
