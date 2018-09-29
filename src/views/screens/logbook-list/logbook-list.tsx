4a178d75940ffae6710112d235f9083024d545c2
Robert Cooper <robert.cooper.qu@gmail.com>
2018-09-29 17:17:24 -0400

    Clean up logbook-list view code

--- src/views/screens/logbook-list/logbook-list.tsx
+++ src/views/screens/logbook-list/logbook-list.tsx
@@ -1,43 +1,29 @@
 import * as React from "react"
-import { inject, observer } from "mobx-react"
-import { ViewStyle } from "react-native"
 import { NavigationScreenProps } from "react-navigation"
+import { ViewStyle } from "react-native"
+import { inject, observer } from "mobx-react"
 
 import { AddEntryButton } from "src/views/shared/add-entry-button"
 import { EntryGroup } from "src/views/shared/entry-group"
+import { EntryStoreModel } from "src/models/entry-store"
 import { Screen } from "src/views/shared/screen"
 import { color } from "src/theme"
-import { EntryStoreModel } from "src/models/entry-store"
-
-export interface LogbookListScreenProps extends NavigationScreenProps<{}> {
-  entryStore: typeof EntryStoreModel.Type
-}
 
 const ROOT: ViewStyle = {
   backgroundColor: color.background,
   paddingBottom: 75,
 }
 
+export interface LogbookListScreenProps extends NavigationScreenProps<{}> {
+  entryStore: typeof EntryStoreModel.Type
+}
+
 @inject("entryStore")
 @observer
 export class LogbookList extends React.Component<LogbookListScreenProps, {}> {
-  cleanData = entry => {
-    return {
-      ...entry,
-      weight: `${entry.weightLbs} lbs`,
-    }
-  }
   renderEntries = () => {
     return this.props.entryStore.entries.map(entryGroup => {
-      return (
-        <EntryGroup
-          key={entryGroup.date}
-          date={entryGroup.date}
-          entries={entryGroup.data.map(entry => {
-            return this.cleanData(entry)
-          })}
-        />
-      )
+      return <EntryGroup key={entryGroup.date} date={entryGroup.date} entries={entryGroup.data} />
     })
   }
   render() {
