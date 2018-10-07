21359fefa448b237efb1b180e67a615d1b9bcf11
Robert Cooper <robert.cooper.qu@gmail.com>
2018-10-06 23:30:26 -0400

    Added App Centre post build script

---
+++ appcenter-post-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Upload directly to itunes connect
+if [ “$APPCENTER_BRANCH” == master ];
+then
+sudo gem install fastlane
+sudo gem install gh_inspector
+cd $APPCENTER_SOURCE_DIRECTORY
+cd ios
+bundle exec fastlane ios appcenterPost password:$PASSWORD user:$USER ipa:$APPCENTER_OUTPUT_DIRECTORY/MeatJournal.ipa
+fi
