2b586c05dcaefed869abe54829916e42fb5e951b
Robert Cooper <robert.cooper.qu@gmail.com>
2018-10-07 15:13:22 -0400

    Change app version so see if all hell breaks loose

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-echo "Running the post build script in the app center!"gst
+echo "Running the post build script in the app center!"
+
+# TODO, figure out how to stop the git clone from hanging up with Fastlane Match
 
 # Upload directly to itunes connect
-if [ "$APPCENTER_BRANCH" == "master" ];
-then
-    sudo gem install fastlane
-    sudo gem install gh_inspector
-    cd $APPCENTER_SOURCE_DIRECTORY
-    cd ios
-    bundle exec fastlane ios appcenterPost password:$PASSWORD user:$USER ipa:$APPCENTER_OUTPUT_DIRECTORY/MeatJournal.ipa
-fi
+# if [ "$APPCENTER_BRANCH" == "master" ];
+# then
+#     sudo gem install fastlane
+#     sudo gem install gh_inspector
+#     cd $APPCENTER_SOURCE_DIRECTORY
+#     cd ios
+#     bundle exec fastlane ios appcenterPost password:$PASSWORD user:$USER ipa:$APPCENTER_OUTPUT_DIRECTORY/MeatJournal.ipa
+# fi
